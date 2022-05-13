0aa7f6e289faf55587ff4497e1dfd01d1afbe5a5
Shouhei Takai <shouheitakai.me@gmail.com>
2022-05-14 07:33:38 +0900

    sassの基本実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { LpHeader } from '@/components/organisms/lp-header'
 
 function App() {
   return (
     <section>
+      <LpHeader />
       <h2>コンテンツだよん</h2>
     </section>
   );
